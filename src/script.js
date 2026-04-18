3546a7c47e955f99d758412b0f50a75071c8423c
Danilo Andres Doria Diaz <danilodoria519@gmail.com>
2026-04-18 14:33:54 -0500

    fix some relative measures and grid

--- src/script.js
+++ src/script.js
@@ -1,6 +1,11 @@
-const button = document.getElementById("menuToggle");
-const menu = document.getElementById("navMenu");
+// add the id "menu-toggle" in a constant called button
+const button = document.getElementById("menu-toggle");
 
+// add the id "mav-menu" in a constant called menu
+const menu = document.getElementById("nav-menu");
+
+// Whe the burger button is clicked
+// toggle the "active" class on the menu to show or hide it
 button.addEventListener("click", () => {
     menu.classList.toggle("active");
 });
